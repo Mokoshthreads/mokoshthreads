edb9f2aabf1da3e603aa1bf425326bbf9b0cba6f
Mokoshthreads <mokoshthreads@gmail.com>
2026-04-09 14:58:08 +0800

    Update stickers.js

--- stickers/stickers.js
+++ stickers/stickers.js
@@ -5,8 +5,8 @@ const lightboxImg = document.getElementById("lightboxImage");
 const backdrop = document.getElementById("lightboxBackdrop");
 const closeBtn = document.getElementById("lightboxClose");
 
-// Generate 13 stickers automatically
-for (let i = 1; i <= 13; i++) {
+// Generate 12 stickers automatically
+for (let i = 1; i <= 12; i++) {
   const img = document.createElement("img");
 
   const num = String(i).padStart(2, "0");
